2e62864736aaa546a2ee44e30e9b2e4953eac826
Caleb Thill <calebnthill@gmail.com>
2014-07-04 13:08:07 -0400

    added a log in helper to spec

---
+++ spec/support/log_in_helper.rb
@@ -0,0 +1,9 @@
+module LogInHelper
+  def log_in(user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+  end
+end
