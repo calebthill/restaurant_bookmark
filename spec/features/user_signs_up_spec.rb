d267d1649dd76f3e7a74f5c265bf692b93b172e9
Caleb Thill <calebnthill@gmail.com>
2014-07-04 13:01:07 -0400

    Added devise

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -4,38 +4,36 @@ feature "User can log in or sign up" do
 
   scenario "User can sign up sucessfully" do
     user = FactoryGirl.create(:user)
-    visit root_path
+    visit new_user_registration_path
 
-    click_on("Sign Up")
+    click_on "Sign Up"
 
-    fill_in("First Name"), with: user.first_name
-    fill_in("Last Name"), with: user.last_name
-    fill_in("Email"), with: user.email
-    fill_in("Password"), with: user.password
-    fill_in("Password confirmation"), with: user.password
+    fill_in "First name", with: user.first_name
+    fill_in "Last name", with: user.last_name
+    fill_in "Email", with: "calebnthill123456@gmail.com"
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password
 
-    click_button("Submit")
+    click_button("Sign up")
 
-    expect(page).to have_content("Thanks for signing up!")
+    expect(page).to have_content("Welcome! You have signed up successfully.")
     expect(page).to have_content("Sign Out")
   end
 
   scenario "User does not fill out the form correctly" do
     user = FactoryGirl.create(:user)
-    visit root_path
+    visit new_user_registration_path
 
-    click_on("Sign Up")
+    click_on "Sign Up"
 
-    fill_in("First Name"), with: user.first_name
-    fill_in("Last Name"), with: user.last_name
-    fill_in("Password"), with: user.password
-    fill_in("Password confirmation"), with: user.password
+    fill_in "First name", with: user.first_name
+    fill_in "Last name", with: user.last_name
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password
 
-    click_button("Submit")
+    click_button("Sign up")
 
-    expect(page).to have_content("Im sorry, that didn't go through!")
-    expect(page).to have_content("must specify an email")
+    expect(page).to have_content("Email can't be blank")
     expect(page).to_not have_content("Thanks for signing up!")
-
   end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:first_name) { |n| "The Beanery#{n}" }
     first_name "John"
     last_name "Goodwin"
-    sequence(:email) { |n| "test#{n}@gmail.com" }
+    sequence(:email) { |n| "calebthriller#{n}@gmail.com" }
     password "123456789"
     password_confirmation "123456789"
   end
