b7771ef64f13fab9feaf31e0061febf4df858a43
Caleb Thill <calebnthill@gmail.com>
2014-07-06 13:03:15 -0400

    finished the comments for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -20,6 +20,8 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @comment = Comment.new
+    # @comments =  Comment.where(restaurant_id: params[:id])
+    # binding.pry
   end
 
   private
