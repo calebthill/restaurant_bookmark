1c45fb8bb3da472fd213856d1438d957ade3b120
Caleb Thill <calebnthill@gmail.com>
2014-07-06 14:51:25 -0400

    .

--- app/controllers/favorite_restaurants_controller.rb
+++ app/controllers/favorite_restaurants_controller.rb
@@ -2,5 +2,6 @@ class FavoriteRestaurantsController < ApplicationController
   def index
     @restaurant = Restaurant.new
     @favorite_restaurants = current_user.favorite_restaurants
+    #
   end
 end
