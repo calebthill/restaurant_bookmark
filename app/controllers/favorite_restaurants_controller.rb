5b243d43309ce8b13110b3f16aba741cd0725d95
Caleb Thill <calebnthill@gmail.com>
2014-07-06 14:56:38 -0400

    cleaned up some of the code

--- app/controllers/favorite_restaurants_controller.rb
+++ app/controllers/favorite_restaurants_controller.rb
@@ -2,6 +2,5 @@ class FavoriteRestaurantsController < ApplicationController
   def index
     @restaurant = Restaurant.new
     @favorite_restaurants = current_user.favorite_restaurants
-    #
   end
 end
