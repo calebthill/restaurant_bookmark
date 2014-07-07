e519f93bbfec759f7f97f6cfd71895f5840c5f69
Caleb Thill <calebnthill@gmail.com>
2014-07-07 17:01:04 -0400

    Added test for favoriting a restaurant

---
+++ spec/features/user_adds_a_favorite_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'User clicks a favorite button on someones favorite restaurant to make it their favorite' do
+  # Acceptance Criteria:
+
+  # -Must be logged in
+  # -Must be on the restaurants show page
+  # -Must click the favorite button
+  # -When I click the favorite button I am notified that it has been added to my favorites.
+
+  scenario 'User clicks the favorite button and the restaurant is added to their favorites' do
+    user = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user)
+    restaurant = FactoryGirl.create(:restaurant, user: user)
+    favorite_restauant_count = FavoriteRestaurant.count
+
+    log_in(user2)
+    click_link("All Restaurants")
+    click_link restaurant.name
+
+    click_on "Add to Favorites"
+
+    expect(page).to have_content("Restaurant Added to favorites")
+    expect(favorite_restauant_count).to eq(favorite_restauant_count + 1)
+    expect(page).to_not have_content("Add to Favorites")
+  end
+end
