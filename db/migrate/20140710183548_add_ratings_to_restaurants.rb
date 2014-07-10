8d6935b9fa1c26c1f26465a6f6d3dcf3c3ada999
Caleb Thill <calebnthill@gmail.com>
2014-07-10 14:38:14 -0400

    added ratings to restaurants table

---
+++ db/migrate/20140710183548_add_ratings_to_restaurants.rb
@@ -0,0 +1,5 @@
+class AddRatingsToRestaurants < ActiveRecord::Migration
+  def change
+    add_column :restaurants, :rating, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706154308) do
+ActiveRecord::Schema.define(version: 20140710183548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140706154308) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rating"
   end
 
   create_table "users", force: true do |t|
