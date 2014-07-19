3a7a3b43b6526f632b96ebdae879c9f6371cca16
Caleb Thill <calebnthill@gmail.com>
2014-07-19 15:42:21 -0400

    added a test for user searching for the name of the restaurant

--- spec/features/user_searches_through_favorites_spec.rb
+++ spec/features/user_searches_through_favorites_spec.rb
@@ -6,11 +6,25 @@ feature 'User can search through their favorite restaurant by category or by nam
     5.times {FactoryGirl.create(:restaurant, user: user)}
     restaurant = FactoryGirl.create(:restaurant, name: "The Farmhouse", category: "American", user: user)
 
-
     visit root_path
     log_in(user)
 
     fill_in "search", with: "American"
+    click_on "Search"
+
+    expect(page).to have_content("The Farmhouse")
+    expect(page).to_not have_content("Little Italia")
+  end
+
+  scenario 'User searches for the name of the restaurant' do
+    user = FactoryGirl.create(:user)
+    5.times {FactoryGirl.create(:restaurant, user: user)}
+    restaurant = FactoryGirl.create(:restaurant, name: "The Farmhouse", category: "American", user: user)
+
+    visit root_path
+    log_in(user)
+
+    fill_in "search", with: "The Farmhouse"
     click_on "Search"
 
     expect(page).to have_content("The Farmhouse")
