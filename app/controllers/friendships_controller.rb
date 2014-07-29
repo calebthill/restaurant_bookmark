c868b49fbf2ad589f7eeb4d4b67461403345d1b7
Caleb Thill <calebnthill@gmail.com>
2014-07-29 15:34:32 -0400

    connected view and controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,13 @@
-class FriendshipController < ApplicationController
+class FriendshipsController < ApplicationController
   def create
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    # binding.pry
+    if @friendship.save
+      flash[:notice] = "Added as friend"
+      redirect_to favorite_restaurants_path
+    else
+      render :new
+    end
   end
 
   def destroy
