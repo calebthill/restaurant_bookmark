f8a98477723dfb0670352d0a908576ea4f1f99b1
Caleb Thill <calebnthill@gmail.com>
2014-07-30 09:32:02 -0400

    added inverse_friendships association to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
   has_many :comments
   has_many :favorite_restaurants
   has_many :restaurants, through: :favorite_restaurants
