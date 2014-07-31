c7f0550673b48202459ce58a1c94b4f5e1ab605a
Caleb Thill <calebnthill@gmail.com>
2014-07-31 11:23:33 -0400

    outline of following test

---
+++ spec/features/user_views_followers_restaurant_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+feature "user views another persons favorite restaurant (they have to be following them)" do
+  scenario "User clicks on a user and can see their favorite restaurants" do
+
+  end
+end
