46b8ec95b975100faee0e28a792a0af89f466b3a
Caleb Thill <calebnthill@gmail.com>
2014-08-03 13:08:56 -0400

    added another test for searching for a partial name

--- spec/features/user_searches_for_other_users_spec.rb
+++ spec/features/user_searches_for_other_users_spec.rb
@@ -26,5 +26,18 @@ feature "User searches for a user" do
     expect(page).to have_content("Caleb Goodwin")
     expect(page).to_not have_content("Wendel Thill")
   end
+
+  scenario "User fills in search form with a users partial first name" do
+    5.times { FactoryGirl.create(:user, last_name: "Thill") }
+    caleb = FactoryGirl.create(:user, first_name: "Caleb")
+
+    log_in(caleb)
+    click_on "Users"
+    fill_in "search", with: "Goodw"
+    click_on "Search"
+
+    expect(page).to have_content("Caleb Goodwin")
+    expect(page).to_not have_content("Wendel Thill")
+  end
 end
 
